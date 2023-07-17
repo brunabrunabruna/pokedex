63eda2b75f569809cba35983b004530c74a6b58d
brunabrunabruna <34475264+brunabrunabruna@users.noreply.github.com>
2023-07-17 15:24:49 +0200

    added loop

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
-alert("hello world");
+let pokemonList = [];
+pokemonList = [
+  { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"] },
+  { name: "Ivysaur", height: 1.2, types: ["grass", "poison"] },
+  { name: "Charmander", height: 0.6, types: ["fire"] },
+  { name: "Squirtle", height: 0.5, types: ["water"] },
+];
+let bigPokemon = "wow thats big!";
+let pokemonInfo;
 
-let favoriteFood = "sushi";
-document.write(favoriteFood);
+for (let i = 0; i < pokemonList.length; i++) {
+  pokemonInfo =
+    pokemonList[i].name + ` ` + `(height: ${pokemonList[i].height})`;
+  if (pokemonList[i].height > 1) {
+    document.write(pokemonInfo + bigPokemon + `</br>`);
+  } else {
+    document.write(pokemonInfo + `</br>`);
+  }
+}
--- script.js
+++
@@ -1,7 +0,0 @@
-let pokemonList = [];
-pokemonList = [
-  { name: "Bulbasaur", height: 7, types: ["grass", "poison"] },
-  { name: "Ivysaur", height: 1, types: ["grass", "poison"] },
-  { name: "Charmander", height: 0.6, types: ["fire"] },
-  { name: "Squirtle", height: 0.5, types: ["water"] },
-];
