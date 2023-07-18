5a4d1e3f269aebc02ec9da15d46cb16a2edca353
brunabrunabruna <34475264+brunabrunabruna@users.noreply.github.com>
2023-07-18 15:45:23 +0200

    IIFE function refactoring

--- js/script.js
+++ js/script.js
@@ -1,12 +1,30 @@
-let pokemonList = [];
-pokemonList = [
-  { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"] },
-  { name: "Ivysaur", height: 1.2, types: ["grass", "poison"] },
-  { name: "Charmander", height: 0.6, types: ["fire"] },
-  { name: "Squirtle", height: 0.5, types: ["water"] },
-];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"] },
+    { name: "Ivysaur", height: 1.2, types: ["grass", "poison"] },
+    { name: "Charmander", height: 0.6, types: ["fire"] },
+    { name: "Squirtle", height: 0.5, types: ["water"] },
+  ];
 
-pokemonList.forEach(function (item) {
+  function add(newPokemon) {
+    if (typeof newPokemon === "object") {
+      pokemonList.push(newPokemon);
+    } else console.log("wrong input");
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return { add: add, getAll: getAll };
+})();
+
+//adds new pokemon
+pokemonRepository.add({ name: "pikachu", height: 0.5, types: ["water"] });
+console.log(pokemonRepository.getAll());
+
+//writes pokemon list
+pokemonRepository.getAll().forEach(function (item) {
   let pokemonInfo = item.name + `  (height: ${item.height})`;
   let bigPokemon = "  -wow thats big!";
 
