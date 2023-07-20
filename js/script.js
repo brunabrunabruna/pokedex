24401ae6a592c7c1ced23a148aab7dbc317b441f
brunabrunabruna <brunaandreis@hotmail.com>
2023-07-20 13:12:58 +0200

    add event listener & html items

--- js/script.js
+++ js/script.js
@@ -6,31 +6,63 @@ let pokemonRepository = (function () {
     { name: "Squirtle", height: 0.5, types: ["water"] },
   ];
 
+  //adds a new pokemon to the end of the list
   function add(newPokemon) {
     if (typeof newPokemon === "object") {
-      pokemonList.push(newPokemon);
+      let newPokemonKeys = Object.keys(newPokemon);
+      let pokemonListKeys = Object.keys(pokemonList[0]);
+
+      if (newPokemonKeys.length === pokemonListKeys.length) {
+        for (let i = 0; i < newPokemonKeys.length; i++) {
+          if (pokemonListKeys.includes(newPokemonKeys[i]) === false) {
+            return false;
+          }
+        }
+
+        pokemonList.push(newPokemon);
+      } else {
+        console.log("wrong input");
+      }
     } else console.log("wrong input");
   }
-
+  //returns whole pokemon list
   function getAll() {
     return pokemonList;
   }
 
-  return { add: add, getAll: getAll };
+  //logs pokemon name to console
+  function showDetails(pokemon) {
+    console.log(pokemon.name);
+  }
+
+  //adds html element (li & button) to every pokemon of the list
+  function addListItem(pokemon) {
+    let pokemonUl = document.querySelector(".pokemon-list");
+    //creates li and button
+    let listItem = document.createElement("li");
+    let button = document.createElement("button");
+    //listens for click, then calls function when clicked
+    button.addEventListener("click", function () {
+      showDetails(pokemon);
+    });
+    // sets button text to the name of the pokemon
+    button.innerText = pokemon.name;
+    //styling of the button
+    button.classList.add("button");
+
+    listItem.appendChild(button);
+    pokemonUl.appendChild(listItem);
+  }
+
+  //returns all the functions that can be accessed from outside of main function here
+  return { add: add, getAll: getAll, addListItem: addListItem };
 })();
 
-//adds new pokemon
+//adds new pokemon to the list
 pokemonRepository.add({ name: "pikachu", height: 0.5, types: ["water"] });
 console.log(pokemonRepository.getAll());
 
-//writes pokemon list
+//generates new li and button itens for each pokemon on the list
 pokemonRepository.getAll().forEach(function (item) {
-  let pokemonInfo = item.name + `  (height: ${item.height})`;
-  let bigPokemon = "  -wow thats big!";
-
-  if (item.height > 1) {
-    document.write(`<p>` + pokemonInfo + bigPokemon + `</p>`);
-  } else {
-    document.write(`<p>` + pokemonInfo + `</p>`);
-  }
+  pokemonRepository.addListItem(item);
 });
